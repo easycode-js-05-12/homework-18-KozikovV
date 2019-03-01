a78911e063fad30d6b4df2ef48eedfca25cb9b9f
kozikov <kozikov.ivan1@gmail.com>
2019-03-01 13:24:07 +0200

    start

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+import { LoginComponent } from './components/login.component';
+import { HomeComponent } from './components/home.component';
+import { NotFoundComponent } from './components/notfound.component';
+import { UserComponent } from './components/user.component';
+import { ActiveRoute } from './core/active.route.service';
+
+const routes = {
+    '/': new HomeComponent(),
+    '/login': new LoginComponent(),
+    '/users/:id': new UserComponent(),
+    '**': new NotFoundComponent()
+};
+
+const activeRoute = new ActiveRoute();
+
+const router = async () => {
+    const container = document.querySelector('app-container');
+    const request = activeRoute.parseRequestURL();
+    const url = (request.resourse ? '/' + request.resourse : '/') + (request.id ? '/:id' : '');
+
+    const component = routes[url] || routes['**']; 
+    
+    await component.beforeRender();
+    container.innerHTML = component.render();
+    component.afterRender();
+}
+
+window.addEventListener('load', router);
+window.addEventListener('hashchange', router);
+
+
+
